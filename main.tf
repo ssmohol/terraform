7a9549045f77ff4c089f6b39f2c46ec30060d9ab
Shekhar Mohol <83066123+ssmohol@users.noreply.github.com>
2023-04-12 10:10:55 +0530

    First Commit - Deploy AzureResourceGroup

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+	backend "azurerm" {}
+	}
+
+provider "azurerm" {
+	version = "=2.0.0"
+	features {}
+	}
+
+resource "azurerm_resource_group" "rg" {
+	name = var.resource_group_name
+	location = var.location
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "terrform-demo-rg"
+location = "westeurope"
